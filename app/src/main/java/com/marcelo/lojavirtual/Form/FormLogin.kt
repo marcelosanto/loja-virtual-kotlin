191df38d560dbf6a7bf182395c4a814c318cb324
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-21 15:17:15 -0300

    fix: finish screen login

--- app/src/main/java/com/marcelo/lojavirtual/Form/FormLogin.kt
+++ app/src/main/java/com/marcelo/lojavirtual/Form/FormLogin.kt
@@ -2,11 +2,14 @@ package com.marcelo.lojavirtual.Form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.marcelo.lojavirtual.R
 
 class FormLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_login)
+
+        supportActionBar!!.hide()
     }
 }
