15aea27bc9072db51c22be419c66b7d88d5fe20a
Marcelo Santos <rocha.mer21@gmail.com>
2021-05-20 12:21:10 -0300

    criada a função que seleciona a foto da galeria

--- app/src/main/java/com/marcelo/lojavirtual/Fragments/CadastroProdutos.kt
+++ app/src/main/java/com/marcelo/lojavirtual/Fragments/CadastroProdutos.kt
@@ -1,12 +1,45 @@
 package com.marcelo.lojavirtual.Fragments
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.marcelo.lojavirtual.R
+import com.marcelo.lojavirtual.databinding.ActivityCadastroProdutosBinding
+import com.marcelo.lojavirtual.databinding.ActivityFormLoginBinding
 
+@Suppress("DEPRECATION")
 class CadastroProdutos : AppCompatActivity() {
+    private lateinit var binding: ActivityCadastroProdutosBinding
+
+    private var SelecionarUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro_produtos)
+        binding = ActivityCadastroProdutosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btSelecionarFoto.setOnClickListener {
+            selecionarFotoDaGaleria()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0) {
+            SelecionarUri = data?.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, SelecionarUri)
+            binding.fotoProduto.setImageBitmap(bitmap)
+            binding.btSelecionarFoto.alpha = 0f
+        }
+    }
+
+    private fun selecionarFotoDaGaleria() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent,0)
     }
 }
