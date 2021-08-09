478f6d289c0cd7dfe8a2991ddc17be5cfaaa41a4
Marcelo Santos <rocha.mer21@gmail.com>
2021-08-09 00:39:32 -0300

    Listando Dados

--- app/src/main/java/com/marcelo/lojavirtual/Fragments/Produtos.kt
+++ app/src/main/java/com/marcelo/lojavirtual/Fragments/Produtos.kt
@@ -1,34 +1,24 @@
 package com.marcelo.lojavirtual.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.firebase.firestore.FirebaseFirestore
+import com.marcelo.lojavirtual.Model.Dados
 import com.marcelo.lojavirtual.R
+import com.marcelo.lojavirtual.databinding.FragmentProdutosBinding
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Produtos.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Produtos : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    private lateinit var Adapter: GroupAdapter<ViewHolder>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +28,51 @@ class Produtos : Fragment() {
         return inflater.inflate(R.layout.fragment_produtos, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Produtos.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Produtos().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private var fragmentProdutos: FragmentProdutosBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val bindingProdutos = FragmentProdutosBinding.bind(view)
+        fragmentProdutos = bindingProdutos
+
+        val recycler_produtos = bindingProdutos.recyclerProdutos
+
+        Adapter = GroupAdapter()
+        recycler_produtos.adapter = Adapter
+        Adapter.setOnItemClickListener { item, view ->
+            Toast.makeText(context, "Item Clicado", Toast.LENGTH_LONG).show()
+        }
+
+        BuscarProdutos()
+    }
+
+    private inner class ProdutosItem(internal val adProducts: Dados) : Item<ViewHolder>() {
+        override fun getLayout(): Int {
+            return R.layout.lista_produtos
+        }
+
+        override fun bind(viewHolder: ViewHolder, position: Int) {
+
+            viewHolder.itemView.findViewById<TextView>(R.id.nomeProduto).text = adProducts.nome
+            viewHolder.itemView.findViewById<TextView>(R.id.precoProduto).text = adProducts.preco
+            var fotoProduto = viewHolder.itemView.findViewById<ImageView>(R.id.imagemProduto)
+            Picasso.get().load(adProducts.uid).into(fotoProduto)
+        }
+    }
+
+    private fun BuscarProdutos() {
+        FirebaseFirestore.getInstance().collection("Produtos")
+            .addSnapshotListener { snapshot, exception ->
+                exception?.let {
+                    return@addSnapshotListener
+                }
+                snapshot?.let {
+                    for (doc in snapshot) {
+                        val produtos = doc.toObject(Dados::class.java)
+                        Adapter.add(ProdutosItem(produtos))
+                    }
                 }
             }
+
     }
 }
+
